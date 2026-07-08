1c4e4690aa108c42d2c4d1e763eef0b303fe176a
Swati Chauhan <sc255347@gmail.com>
2026-07-08 17:41:50 +0530

    Create Resume page with form and preview layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-
+<link
+  href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
+  rel="stylesheet"></link>
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ResumeTemplate from "../pages/ResumeTemplate";
+import Personal from "../pages/Personal";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ResumeTemplate />} />
+        <Route path="/personal" element={<Personal />} />
       </Routes>
     </BrowserRouter>
   );
