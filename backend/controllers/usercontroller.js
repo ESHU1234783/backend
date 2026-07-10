e34ad293f78489e383050445b2d3b619ea947886
Akshay N <akshaynigam405@gmail.com>
2026-07-10 15:22:06 +0530

    the experience page API is functional

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -23,44 +23,39 @@ export const createresume = async (req, res) => {
 
 export const experience = async (req, res) => {
     try {
-        const {
-            company,
-            role,
-            start,
-            end,
-            accomplishment
-        } = req.body
-
-
-        const exp = await usermodel.findByIdAndUpdate(req.params.id, {
-            company: req.body.company,
-            role: req.body.role,
-            start: req.body.start,
-            end: req.body.end,
-            accomplishment: req.body.accomplishment,
-            experienceCompleted: true
-
-
-        },
+        const exp = await usermodel.findByIdAndUpdate(
+            req.params.id,
+            {
+                $push: {
+                    experience: {
+                        company: req.body.company,
+                        role: req.body.role,
+                        start: req.body.start,
+                        end: req.body.end,
+                        accomplishment: req.body.accomplishment
+                    }
+                },
+                experienceCompleted: true
+            },
             {
                 new: true
-            });
+            }
+        );
 
-            res.status(200).json({
-                message:"succesful",
-                data:exp
-            });
-            console.log(exp);
-            console.log(req.params.id);
-            console.log();
-            
-    }
-    catch (err) {
+        res.status(200).json({
+            message: "successful",
+            data: exp
+        });
+
+        console.log(exp);
+        console.log(req.params.id);
+
+    } catch (err) {
         console.log(err);
+
         res.status(500).json({
             message: "Went wrong",
             error: err.message
-        })
+        });
     }
-
-}
+};
