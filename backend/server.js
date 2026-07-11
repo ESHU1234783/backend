720e5071d573129acb192a3f6876aeaf6e13e95f
Akshay N <akshaynigam405@gmail.com>
2026-07-11 14:48:12 +0530

    save local changes

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use("/api",route);
 
 //connection using port saved in .env file
 DB();
-const PORT= process.env.PORT || 3000;
+const PORT= process.env.PORT ;
 app.listen(PORT,() =>{
     console.log(`server is running on ${PORT}`);
 });
